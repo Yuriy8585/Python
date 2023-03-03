e85e2033257be0b5f4e67e198a46e2e1a1c3c329
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2023-03-03 14:55:10 +0100

    task

--- Homework5/26.py
+++ Homework5/26.py
@@ -6,3 +6,16 @@
 A = 3; B = 5 -> 243 (3⁵)
     A = 2; B = 3 -> 8
 """
+num = int(input('input A '))
+num2 = int(input("input B "))
+
+def check_num(num, num2):
+    if num2 >= 1:
+        return num**num2
+    elif num2 <= -1:
+        return num**num2
+    else:
+        return zero
+
+zero = 1
+print(f'Результат возведения {num} в степень {num2} равен 3{check_num(num, num2)}')
